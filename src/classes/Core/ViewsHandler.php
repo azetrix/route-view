b33624b164d0afb841054f265fd7288f5f762835
Phoenix Eve Aspacio <aspaciop@gmail.com>
2018-12-29 06:39:10 +0800

    Update

--- src/classes/Core/ViewsHandler.php
+++ src/classes/Core/ViewsHandler.php
@@ -6,7 +6,7 @@ class ViewsHandler
 {
 
     public static function yield(string $view_file) {
-        $view_file = Commons::file_build_path(__DIR__, '..', '..', '..', 'views', $view_file);
+        $view_file = Commons::file_build_path(__DIR__, '..', '..', 'views', $view_file);
         if (file_exists($view_file)) {
             include_once($view_file);
         } else {
